97b40f99a6aa04bc450d53f0c59fd59fc26fdb54
Cali2016-Pad <iztla1996@gmail.com>
2025-10-06 18:02:04 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    const pronoun = ["the", "our"];
+    const adj = ["big", "small", "huge", "tiny"];
+    const noun = ["dog", "runner", "flower", "cat"];
+    const extension = [".com", ".net"];
+    const combinations = [];
+        for (const p of pronoun) {
+            for (const a of adj){
+                for (const n of noun){
+                    for (const e of extension){
+                        combinations.push(p +  a + n + e);
+                    }
+                }
+            }
+        }
+   combinations.forEach(domain => console.log(domain));
+}
